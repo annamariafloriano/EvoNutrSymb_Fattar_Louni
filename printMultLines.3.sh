ab00c68c171181f547cfbc4c078ad9feca273a92
annamariafloriano <86775791+annamariafloriano@users.noreply.github.com>
2024-09-10 18:22:50 +0200

    Add files via upload

---
+++ printMultLines.3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# The string you want to print
+your_string="SelClusters/"
+
+# Number of lines you want to print the string
+num_lines=438
+
+# Loop to print the string 438 times
+for ((i=0; i<$num_lines; i++)); do
+    echo $your_string
+done
